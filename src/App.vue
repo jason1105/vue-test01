2ecc8f40041d2b54fdec018491ddb621ddb46548
lvwei <13366824@qq.com>
2018-09-21 19:07:33 +0800

    add Bind2-Input

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <CustomInput v-2nd-bind="msgName" msg="Welcome to Your Vue.js App"  v-model="msgId"/>
+        <h1>I am msgId in msgId: {{ msgId }}</h1>
+        <h1>I am msgName in parent: {{ msgName }}</h1>
+
+        <CustomInput v-2nd-bind="msgObj.name" msg="Welcome to Your Vue.js App"  v-model="msgObj.id"/>
+        <h1>I am msgId in msgId: {{ msgObj.id }}</h1>
+        <h1>I am msgName in parent: {{ msgObj.name }}</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import CustomInput from './components/Bind2-Input.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            CustomInput
+        },
+        data() {
+            return {
+                msgId: 1,
+                msgName: "no name",
+                msgObj: {id: 2, name: ""}
+            }
+        }
+    }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Bind2-Input.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="hello">
+        <input v-focus v-bind:value="value" v-on:input="onInputHandler"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Bind2-Input',
+        props: {
+            value: ""
+        },
+        data: function() {
+            return {
+                specName: ""
+            }
+        },
+        methods: {
+            onInputHandler: function(event) {
+                this.specName = event.target.value + "-name";
+
+                this.$emit('2ndEvent', event.target.value + "-name");
+                this.$emit('input', event.target.value);
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
